f41386fd8095c3d874b7948d3596ea4fdf69b9c6
RicardoSalambi <ricardox200@gmail.com>
2020-01-10 15:01:56 +0200

    Updates

---
+++ precache-manifest.efb866426867b2a277de8ae9e5f5c66f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d8aac6e53d3b9ebf0b558ba561d285",
+    "url": "/Curriculum-Vitae/index.html"
+  },
+  {
+    "revision": "357c6d322a075aa12169",
+    "url": "/Curriculum-Vitae/static/css/2.7abef457.chunk.css"
+  },
+  {
+    "revision": "050ce2b357d2e1fae384",
+    "url": "/Curriculum-Vitae/static/css/main.2d6d31fa.chunk.css"
+  },
+  {
+    "revision": "357c6d322a075aa12169",
+    "url": "/Curriculum-Vitae/static/js/2.9247db2e.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/Curriculum-Vitae/static/js/2.9247db2e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "050ce2b357d2e1fae384",
+    "url": "/Curriculum-Vitae/static/js/main.d5af4028.chunk.js"
+  },
+  {
+    "revision": "c8c34e66f04882aa2dd3",
+    "url": "/Curriculum-Vitae/static/js/runtime-main.b993e54b.js"
+  },
+  {
+    "revision": "4009645f6236d3a524432f891bfa4b24",
+    "url": "/Curriculum-Vitae/static/media/Article1.4009645f.PNG"
+  },
+  {
+    "revision": "d75531428efa66f0a498f1f5039e5259",
+    "url": "/Curriculum-Vitae/static/media/Article2.d7553142.PNG"
+  },
+  {
+    "revision": "e00ef2f65a1d5def88ce2cc6138e8c49",
+    "url": "/Curriculum-Vitae/static/media/IMG_0343.e00ef2f6.JPG"
+  },
+  {
+    "revision": "dd13a5f7e612441a342c1cd3866a7e72",
+    "url": "/Curriculum-Vitae/static/media/bay-boats-cape-town-cityscape-259447.dd13a5f7.jpg"
+  }
+]);
